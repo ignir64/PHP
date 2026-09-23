c559ce4397a47b8ff5eb96d4c336cab888142f8c
Thibaut Fontaine <thibaut210107@gmail.com>
2026-09-23 10:38:44 +0200

    ertyui

--- TP4/main.php
+++ TP4/main.php
@@ -14,7 +14,7 @@ if ($link) {
 } else {
     print "connexion échouée<br>";
 }
-
+/*
 $query = "SELECT * FROM $nomtable";
 
 $result = mysqli_query($link, $query) or die("Requête impossible");
@@ -29,5 +29,18 @@ while ($donnees = mysqli_fetch_array($result)) {
     $ch2=$donnees['indice'];
     print "ville : $ch1, indice : $ch2<br>";
 }
+*/
+
+header("Content-type: image/jpeg");
+
+//ImageCreate(400, 300) or die("Impossible de créer l'image");
+$test = ImageCreateTrueColor(400, 300) or die("Impossible de créer l'image");
+//$test2 = ImageCreateTrueColor(40, 30) or die("Impossible de créer l'image");
+$color = ImageColorAllocate($test, 255, 0, 255);
+$gamma = Imagegammacorrect($test, 2, 1);
+$color2 = ImageColorAllocate($test2, 255, 0, 255);
+$polygon = imageFilledPolygon($test, array(100, 10, 50, 60, 150, 60), 4, $color2);
+
+$image = imagefilledrectangle($polygon, 0, 0, $color2);
 
 ?>
